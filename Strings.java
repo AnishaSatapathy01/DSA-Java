abeb9f596860a4f87d61fca65b19143e7b6a4c49
AnishaSatapathy01@ <anishasatapathy23@gmail.com>
2025-06-23 09:16:37 +0530

    String basics

---
+++ Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+    public static void main (String args[]){
+        String name ="Anisha";
+        System.out.println(name);
+        // llength of string
+        System.out.println(name.length());
+        //charAt
+        System.out.println(name.charAt(2));
+        for (int i =0; i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+
+
+    }
+}
